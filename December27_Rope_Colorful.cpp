d248c7df032f2ae2c64313a0799c5cace6776f3f
SanskarDhyani98 <sanskardhyani98@gmail.com>
2023-12-28 10:07:35 +0530

    Daily problem

---
+++ December27_Rope_Colorful.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int n = colors.size();
+        vector<vector<int>> dp(n + 1, vector<int> (27, 0));
+        
+        for(int i = n - 1; i >=0; i--){
+            for(int j = 0; j < 27; j++){
+                int remove, noremove;
+
+                remove = neededTime[i] + dp[i + 1][j];
+                noremove = dp[i + 1][colors[i] - 'a' + 1];
+                
+                if(colors[i] - 'a' + 1 == j)
+                    dp[i][j] = remove;
+                else
+                    dp[i][j] = min(remove, noremove);
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
