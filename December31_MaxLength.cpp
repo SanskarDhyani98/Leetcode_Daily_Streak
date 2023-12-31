1d30dba7eb54581756ed12727e6fd621806150c4
SanskarDhyani98 <sanskardhyani98@gmail.com>
2023-12-31 13:36:13 +0530

    POTD solved

--- December31_MaxLength.cpp
+++ December31_MaxLength.cpp
@@ -26,19 +26,43 @@ using namespace std;
 //Approach-1 (Using Brute Force) - ACCEPTED
 //T.C : O(n^2)
 //S.C : O(1)
+// class Solution {
+// public:
+//     int maxLengthBetweenEqualCharacters(string s) {
+//         int result = -1;
+//         int n      = s.length();
+        
+//         for(int i = 0; i < n-1; i++) {
+//             for(int j = i+1; j<n; j++) {
+                
+//                 if(s[i] == s[j]) {
+//                     result = max(result, j-i-1);
+//                 }
+            
+//             }
+//         }
+        
+//         return result;
+//     }
+// };
+//Approach-2 (Using Hashmap) - ACCEPTED
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        int result = -1;
-        int n      = s.length();
+        int n = s.length();
+        unordered_map<char, int> mp;
         
-        for(int i = 0; i < n-1; i++) {
-            for(int j = i+1; j<n; j++) {
-                
-                if(s[i] == s[j]) {
-                    result = max(result, j-i-1);
-                }
+        int result = -1;
+        
+        for(int i = 0 ; i < n;  i++) {
+            char ch = s[i];
             
+            if(mp.find(ch) == mp.end()) {
+                mp[ch] = i;
+            } else {
+                result = max(result, i - mp[ch] - 1);
             }
         }
         
