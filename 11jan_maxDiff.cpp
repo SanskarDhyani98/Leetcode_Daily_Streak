addfb33bd362b97a193e21d52ceb3579b11ff1d6
SanskarDhyani98 <sanskardhyani98@gmail.com>
2024-01-12 11:01:47 +0530

    daily

--- 11jan_maxDiff.cpp
+++ 11jan_maxDiff.cpp
@@ -10,28 +10,69 @@ struct TreeNode {
  };
 class Solution {
 public:
-    int maxD;
-    void maxDiffUtil(TreeNode* root,TreeNode* child)
-    {
-        if(root==NULL||child==NULL)
-            return ;    
-        maxD=max(maxD,abs(root->val-child->val));
-        maxDiffUtil(root,child->left);
-        maxDiffUtil(root,child->right);
-    }
-    void maxDiff(TreeNode* root)
-    {
-         if(root==NULL)
-            return ;
-        maxDiffUtil(root,root->left);
-        maxDiffUtil(root,root->right);
-        maxDiff(root->left);
-        maxDiff(root->right);
-    }
-    int maxAncestorDiff(TreeNode* root) {
-        maxD=-1;
-        maxDiff(root);
-        return maxD;
+    // int maxD;
+    // void maxDiffUtil(TreeNode* root,TreeNode* child)
+    // {
+    //     if(root==NULL||child==NULL)
+    //         return ;    
+    //     maxD=max(maxD,abs(root->val-child->val));
+    //     maxDiffUtil(root,child->left);
+    //     maxDiffUtil(root,child->right);
+    // }
+    // void maxDiff(TreeNode* root)
+    // {
+    //      if(root==NULL)
+    //         return ;
+    //     maxDiffUtil(root,root->left);
+    //     maxDiffUtil(root,root->right);
+    //     maxDiff(root->left);
+    //     maxDiff(root->right);
+    // }
+    // int maxAncestorDiff(TreeNode* root) {
+    //     maxD=-1;
+    //     maxDiff(root);
+    //     return maxD;
+    // }
+
+    //2
+//      int maxAncestorDiff(TreeNode* root) {
+        
+//        return maxDiffUtil(root,root->val,root->val);
+        
+//     }
+//    int maxDiffUtil(TreeNode* root, int minv,int maxv)
+//      {
+//        if(root==NULL)
+//            return abs(minv-maxv);
+//        minv=min(minv,root->val);
+//        maxv=max(maxv,root->val);
+//        int l=maxDiffUtil(root->left,minv,maxv);
+//        int r=maxDiffUtil(root->right,minv,maxv);
+//        return max(l,r);
+//    }
+      int maxAncestorDiff(TreeNode* root) {
+        int ans = 0;
+        const int inf = 1e6;
+        
+        function<pair<int,int>(TreeNode *)> dfs = [&](TreeNode * node) -> pair<int,int> {
+            if(!node)
+                return {inf, -inf};
+            
+            pair<int,int> left = dfs(node -> left);
+            pair<int,int> right = dfs(node -> right);
+            
+            int MIN = min(left.first, right.first);
+            int MAX = max(left.second, right.second);
+            
+            if(node -> left or node -> right)
+                ans = max({ans, abs(MIN - node -> val), abs(MAX - node -> val)});
+            
+            return {min(MIN, node -> val), max(MAX, node -> val)};
+        };
+        
+        dfs(root);
+        
+        return ans;
     }
 };
 int main()
---
+++ 12jan_halfCountVowel.cpp
@@ -0,0 +1,41 @@
+//Approach-1 (without Using Set)
+//T.C : O(n)
+//S.C : O(1)
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isVowel(char &ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+    }
+    bool halvesAreAlike(string s) {
+        int n = s.length();
+        
+        int mid = n/2;
+        
+        int i = 0, j = mid;
+        
+        int countL = 0;
+        int countR = 0;
+        
+        while(i < n/2 && j < n) {
+            if(isVowel(s[i])) countL++;
+            
+            if(isVowel(s[j])) countR++;
+            
+            i++;
+            j++;
+        }
+        
+        
+        return countL == countR;
+    }
+};
+int main()
+{   Solution ob;
+    string s;
+    cin>>s;
+    cout<<ob.halvesAreAlike(s);
+    return 0;
+}
